ef2e9c1d78b4119e067832c1d0db9606aaad2948
durpy <89787577+durpyneko@users.noreply.github.com>
2025-08-23 02:12:05 +0200

    chore: relative image

--- .config/fish/functions/ff.fish
+++ .config/fish/functions/ff.fish
@@ -1,5 +1,5 @@
 function ff --wraps=fastfetch --description 'alias ff=fastfetch'
   # Gw17d24bUAADv_F_edit.png | Gu_frMIXEAA-f6G_crop.png | Gyj8nYZbUAAF5kq_crop_edit_1.2.png | 8f4464930cf254269949ac87dd6d46cb_edit_1.1.png
-  fastfetch -c ~/dotfiles/misc/configs/ff-config.jsonc --logo '/mnt/Nya-storage/homework/Hiroi\ Kikuri/8f4464930cf254269949ac87dd6d46cb_edit_1.1.png' --logo-type kitty-direct --logo-width 40 --logo-height 18 $argv
+  fastfetch -c ~/dotfiles/misc/configs/ff-config.jsonc --logo '~/dotfiles/misc/images/logo.png' --logo-type kitty-direct --logo-width 40 --logo-height 18 $argv
 
 end
