70b6d4459d4cb95246be2bca3221b392648d6f32
durpy <89787577+durpyneko@users.noreply.github.com>
2025-08-30 01:17:31 +0200

    feat: fish styles & env vars
    - minor banner styling change
    - prompt styling

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    export trash=".local/share/Trash/files/"
 end
 
 function fish_greeting
     # fortune | cowsay | lolcat
-    cat ~/dotfiles/misc/ascii/banner.txt | lolcat
+    cat ~/dotfiles/misc/ascii/banner_colored.txt | lolcat
 end
--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,21 +1,20 @@
-function fish_prompt
-        # This is a simple prompt. It looks like
-        # alfa@nobby /path/to/dir $
-        # with the path shortened and colored
-        # and a "#" instead of a "$" when run as root.
-        set -l symbol ' $ '
-        set -l color $fish_color_cwd
-        if fish_is_root_user
-                set symbol ' # '
-                set -q fish_color_cwd_root
-                and set color $fish_color_cwd_root
+function fish_prompt --description 'Informative prompt'
+        #Save the return status of the previous command
+        set -l last_pipestatus $pipestatus
+        set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
+    
+        if functions -q fish_is_root_user; and fish_is_root_user
+                printf '%s@%s %s%s%s# ' $USER (prompt_hostname) (set -q fish_color_cwd_root
+                                                                     and set_color $fish_color_cwd_root
+                                                                     or set_color $fish_color_cwd) \
+                        (prompt_pwd) (set_color normal)
+        else
+                set -l status_color (set_color $fish_color_status)
+                set -l statusb_color (set_color --bold $fish_color_status)
+                set -l pipestatus_string (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
+        
+                printf '[%s] %s%s@%s %s%s %s%s%s \n> ' (date "+%H:%M:%S") (set_color brblue) \
+                        $USER (prompt_hostname) (set_color $fish_color_cwd) $PWD $pipestatus_string \
+                        (set_color normal)
         end
-    
-        echo -n $USER@$hostname
-    
-        set_color $color
-        echo -n (prompt_pwd)
-        set_color normal
-    
-        echo -n $symbol
 end
